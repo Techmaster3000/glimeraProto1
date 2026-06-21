0e1aafaa44e8c78b0e2077b88cc38f73fa11dba9
Techmaster <52240882+Techmaster3000@users.noreply.github.com>
2026-06-21 19:50:26 +0200

    final tweaks

--- scripts/scene_switcher_static.gd
+++ scripts/scene_switcher_static.gd
@@ -29,7 +29,8 @@ func toggle_menu() -> void:
 	if _is_open():
 		close_menu()
 	else:
-		open_menu()
+		if Dialogic.current_timeline == null:
+			open_menu()
 
 func open_menu() -> void:
 	if _is_open():
